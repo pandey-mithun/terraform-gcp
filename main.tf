de710ac9bc336764e22add4d41899ac8922bad65
mithun <pandey.mithun@hotmail.com>
2022-11-06 23:02:54 +0530

    created custom  subnets

--- main.tf
+++ main.tf
@@ -25,3 +25,16 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "vpc-network3"
 }
+
+resource "google_compute_network" "vpc_network_custom" {
+  name                    = "vpc-network-custom"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "network_with_private_secondary_ip_ranges" {
+  for_each      = var.subnet
+  name          = each.key
+  ip_cidr_range = each.value.cidr_block
+  region        = each.value.region
+  network       = google_compute_network.vpc_network_custom.id
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -27,3 +27,14 @@ mapofobject = {
     region     = "us-east"
   }
 }
+
+subnet = {
+  subnet1 = {
+    cidr_block = "10.0.0.0/20"
+    region     = "us-central1"
+  }
+  subnet2 = {
+    cidr_block = "10.2.0.0/20"
+    region     = "europe-west1"
+  }
+}
